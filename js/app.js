90c5ce7bb3713dfbc5903d64e6c2cc4cc77782f6
anderlfrias <anderl.friaas@gmail.com>
2022-03-27 21:35:40 -0400

    Cargando las imagenes desde la API de Picsum

--- js/app.js
+++ js/app.js
@@ -1,13 +1,23 @@
 const url = 'https://picsum.photos/v2/list';
+const imagesContainer = document.getElementById('image_container');
+
 
 console.log("Url: ", url);
+console.log("imagesContainer", imagesContainer);
 
 fetch(url)
 .then((resp) => resp.json())
-.then(function(data) {
-    console.log("data",data);
-    let result = data.results;
-    console.log(result);
+.then(function(images) {
+    let html = '';
+    console.log("data",images);
+    images.forEach(img => {
+        console.log(img.download_url);
+        html += `<figure class="image">
+                    <img src="${img.download_url}" alt="">
+                </figure>`;
+    });
+    console.log(html);
+    imagesContainer.innerHTML = html;
 })
     .catch(function(error) {
     console.log(error);
